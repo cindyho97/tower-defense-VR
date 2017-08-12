b27661494c6451537f074b060a8e8e1808f10586
cindyho97 <cindy.ho8197@gmail.com>
2017-08-12 16:51:10 +0200

    Add Cardboard SDK

---
+++ Tower Defense/Assets/SelectColor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectColor : MonoBehaviour {
+
+    public Material inactiveMaterial;
+    public Material outlineMaterial;
+
+	// Use this for initialization
+	void Start () {
+        SetGazedAt(false);
+	}
+	
+    public void SetGazedAt(bool gazedAt)
+    {
+        if (inactiveMaterial != null && outlineMaterial != null)
+        {
+            GetComponent<Renderer>().material = gazedAt ? outlineMaterial : inactiveMaterial;
+        }
+    }
+
+    public void MoveForward()
+    {
+        transform.Translate(1.0f, 0, 0);
+    }
+}
