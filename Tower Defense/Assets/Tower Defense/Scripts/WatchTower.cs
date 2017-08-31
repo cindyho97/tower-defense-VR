76a6bf9fc98de4b14f47c70c5b9345625d2c6d8e
cindyho97 <cindy.ho8197@gmail.com>
2017-08-31 13:14:22 +0200

    Make watchspot invisible when player is there

--- Tower Defense/Assets/Tower Defense/Scripts/WatchTower.cs
+++ Tower Defense/Assets/Tower Defense/Scripts/WatchTower.cs
@@ -6,20 +6,48 @@ using UnityEngine.EventSystems;
 public class WatchTower : MonoBehaviour, IPointerClickHandler {
 
     private GameObject player;
-    public GameObject cameraPosition;
     private Vector3 cameraStartPosition;
+    private bool playerAtWatchSpot;
+    private Renderer rend;
+    private Collider coll;
+
+    public GameObject cameraPosition;
 
     private void Awake()
     {
         player = GameObject.FindGameObjectWithTag("Player");
         cameraStartPosition = GameObject.FindGameObjectWithTag("CameraStartPosition").transform.position;
+        rend = gameObject.GetComponent<Renderer>();
+        coll = gameObject.GetComponent<Collider>();
         player.transform.position = cameraStartPosition;
+    }
+
+    private void Update()
+    {
+        CheckPlayerPosition();
+    }
+
+    private void CheckPlayerPosition()
+    {
+        if(playerAtWatchSpot && player.transform.position != cameraPosition.transform.position)
+        {
+            playerAtWatchSpot = false;
+            rend.enabled = true;
+            coll.enabled = true;
+        }
     }
 
 
     public void OnPointerClick(PointerEventData eventData)
     {
         player.transform.position = cameraPosition.transform.position;
+        
+        if (gameObject.name.Contains("WatchSpot"))
+        {
+            playerAtWatchSpot = true;
+            rend.enabled = false;
+            coll.enabled = false;
+        }
     }
     
 }
